e929b9ab3d23eb1d70bdcb77626628f218dbfb1b
Marsyas <101160087+0xMarsyas@users.noreply.github.com>
2023-05-23 22:23:38 -0400

    upd8 steps 3-4

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,7 @@
 use std::collections::HashMap;
 
-fn main() {
-    println!("Hello, Coreum!");
-}
-
-// A user can submit a `MultiSend` transaction (similar to bank.MultiSend in cosmos sdk) to transfer multiple
-// coins (denoms) from multiple input addresses to multiple output addresses. A denom is the name or symbol
-// for a coin type, e.g USDT and USDC can be considered different denoms; in cosmos ecosystem they are called
-// denoms, in ethereum world they are called symbols.
-// The sum of input coins and output coins must match for every transaction.
-struct MultiSend {
-    // inputs contain the list of accounts that want to send coins from, and how many coins from each account we want to send.
+pub struct MultiSend {
     inputs: Vec<Balance>,
-    // outputs contains the list of accounts that we want to deposit coins into, and how many coins to deposit into
-    // each account
     outputs: Vec<Balance>,
 }
 
@@ -22,103 +10,113 @@ pub struct Coin {
     pub amount: i128,
 }
 
-struct Balance {
+pub struct Balance {
     address: String,
     coins: Vec<Coin>,
 }
 
-// A Denom has a definition (`CoinDefinition`) which contains different attributes related to the denom:
-struct DenomDefinition {
-    // the unique identifier for the token (e.g `core`, `eth`, `usdt`, etc.)
+pub struct DenomDefinition {
     denom: String,
-    // The address that created the token
     issuer: String,
-    // burn_rate is a number between 0 and 1. If it is above zero, in every transfer,
-    // some additional tokens will be burnt on top of the transferred value, from the senders address.
-    // The tokens to be burnt are calculated by multiplying the TransferAmount by burn rate, and
-    // rounding it up to an integer value. For example if an account sends 100 token and burn_rate is
-    // 0.2, then 120 (100 + 100 * 0.2) will be deducted from sender account and 100 will be deposited to the recipient
-    // account (i.e 20 tokens will be burnt)
     burn_rate: f64,
-    // commission_rate is exactly same as the burn_rate, but the calculated value will be transferred to the
-    // issuer's account address instead of being burnt.
     commission_rate: f64,
 }
 
-// Implement `calculate_balance_changes` with the following requirements.
-// - Output of the function is the balance changes that must be applied to different accounts
-//   (negative means deduction, positive means addition), or an error. the error indicates that the transaction must be rejected.
-// - If sum of inputs and outputs in multi_send_tx does not match the tx must be rejected(i.e return error).
-// - Apply burn_rate and commission_rate as described by their definition.
-// - If the sender does not have enough balances (in the original_balances) to cover the input amount on top of burn_rate and
-// commission_rate, the transaction must be rejected.
-// - burn_rate and commission_rate does not apply to the issuer. So to calculate the correct values you must do this for every denom:
-//      - sum all the inputs coming from accounts that are not an issuer (let's call it non_issuer_input_sum)
-//      - sum all the outputs going to accounts that are not an issuer (let's call it non_issuer_output_sum)
-//      - total burn amount is total_burn = min(non_issuer_input_sum, non_issuer_output_sum)
-//      - total_burn is distributed between all input accounts as: account_share = roundup(total_burn * input_from_account / non_issuer_input_sum)
-//      - total_burn_amount = sum (account_shares) // notice that in previous step we rounded up, so we need to recalculate the total again.
-//      - commission_rate is exactly the same, but we send the calculate value to issuer, and not burn.
-//      - Example:
-//          burn_rate: 10%
-//
-//          inputs:
-//          60, 90
-//          25 <-- issuer
-//
-//          outputs:
-//          50
-//          100 <-- issuer
-//          25
-//          In this case burn amount is: min(non_issuer_inputs, non_issuer_outputs) = min(75+75, 50+25) = 75
-//          Expected burn: 75 * 10% = 7.5
-//          And now we divide it proportionally between all input sender: first_sender_share  = 7.5 * 60 / 150  = 3
-//                                                                        second_sender_share = 7.5 * 90 / 150  = 4.5
-// - In README.md we have provided more examples to help you better understand the requirements.
-// - Write different unit tests to cover all the edge cases, we would like to see how you structure your tests.
-//   There are examples in README.md, you can convert them into tests, but you should add more cases.
-fn calculate_balance_changes(
+// Helper function to find a DenomDefinition for a given denom
+fn find_definition(denom: &String, definitions: &Vec<DenomDefinition>) -> Option<&DenomDefinition> {
+    for definition in definitions {
+        if &definition.denom == denom {
+            return Some(definition);
+        }
+    }
+    None
+}
+
+// Helper function to find a Balance for a given address
+fn find_balance(address: &String, balances: &Vec<Balance>) -> Option<&Balance> {
+    for balance in balances {
+        if &balance.address == address {
+            return Some(balance);
+        }
+    }
+    None
+}
+
+pub fn calculate_balance_changes(
     original_balances: Vec<Balance>,
     definitions: Vec<DenomDefinition>,
-    multi_send_tx: MultiSend,
+    mut multi_send_tx: MultiSend,
 ) -> Result<Vec<Balance>, String> {
-    
-    // First let's verify that the sum of inputs and outputs match for every transaction
+    // Initialize the variables
     let mut input_sum: i128 = 0;
     let mut output_sum: i128 = 0;
 
+    // Calculate sum of inputs and outputs
     for input in &multi_send_tx.inputs {
         for coin in &input.coins {
             input_sum += coin.amount;
         }
     }
-
     for output in &multi_send_tx.outputs {
         for coin in &output.coins {
             output_sum += coin.amount;
         }
     }
 
-    // If sum of inputs and outputs does not match, the transaction must be rejected
+    // Verify sum of inputs and outputs
     if input_sum != output_sum {
         return Err("Sum of inputs and outputs does not match.".to_string());
     }
 
-    // TODO: Apply burn_rate and commission_rate as described by their definition.
-    // TODO: Check if the sender has enough balances to cover the input amount on top of burn_rate and commission_rate.
-    // TODO: Calculate and apply the burn and commission for the issuer.
+    // Calculate and apply burn and commission
+    for input in &mut multi_send_tx.inputs {
+        for coin in &mut input.coins {
+            let definition = find_definition(&coin.denom, &definitions)
+                .ok_or("Could not find DenomDefinition for input coin denom".to_string())?;
+            
+            // Calculate burn and commission
+            let burn = (coin.amount as f64 * definition.burn_rate).round() as i128;
+            let commission = (coin.amount as f64 * definition.commission_rate).round() as i128;
 
-    // If everything is correct, return the original_balances for now
-    Ok(original_balances)
+            // Apply burn and commission
+            coin.amount -= burn;
+            coin.amount -= commission;
+
+            // Add burn and commission to issuer balance
+            // TO-DO: Add logic to find and update the issuer's balance
+        }
+    }
+    
+    // Check if the sender has enough balances to cover the input amount on top of burn_rate and commission_rate
+    for input in &multi_send_tx.inputs {
+        let original_balance = find_balance(&input.address, &original_balances)
+            .ok_or(format!("Could not find original balance for address {}", &input.address))?;
+        
+        for coin in &input.coins {
+            let original_coin = original_balance.coins.iter()
+                .find(|c| c.denom == coin.denom)
+                .ok_or(format!("Could not find original coin for denom {}", &coin.denom))?;
+            
+            if coin.amount > original_coin.amount {
+                return Err("Sender does not have enough balance to cover the input amount on top of burn_rate and commission_rate".to_string());
+            }
+        }
+    }
+
+    // TO-DO 4: Calculate and apply the burn and commission for the issuer
+    Ok(original_balances) // Placeholder: Return the original_balances for now
 }
 
-// TODO: Write different unit tests to cover all the edge cases
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test_calculate_balance_changes() {
-        // TODO: Add your test code here
+        // TO-DO: Add your test code here
     }
+}
+
+fn main() {
+    println!("Hello, Coreum!");
 }
